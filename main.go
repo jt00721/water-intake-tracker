10d054625906ae7ee5664fe1b602c4d3a851f10a
jt00721 <titusgloverj@gmail.com>
2025-01-10 16:32:30 +0000

    Add function to calculate the total water consumed for the day.

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -26,6 +29,47 @@ func logWaterIntake(amount int, filename string) error {
 	return nil
 }
 
+func calculateTotalWater(filename string) (int, error) {
+	// Get logged water intakes from file
+	file, err := os.Open(filename)
+	if err != nil {
+		return 0, fmt.Errorf("could not open file: %v", err)
+	}
+
+	defer file.Close()
+
+	currentDate := time.Now().Format("2006-01-02")
+
+	totalWater := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		parts := strings.Split(line, ",")
+		if len(parts) != 2 {
+			continue
+		}
+
+		timestamp := parts[0]
+		amount, err := strconv.Atoi(parts[1])
+		if err != nil {
+			continue
+		}
+
+		formattedTimeStamp := timestamp[:10]
+		if formattedTimeStamp == currentDate {
+			totalWater += amount
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return 0, fmt.Errorf("error reading file: %v", err)
+	}
+
+	return totalWater, nil
+}
+
 func main() {
 	filename := "water_log.csv"
 	var amount int
@@ -44,4 +88,16 @@ func main() {
 	}
 
 	fmt.Printf("Logged %d ml of water successfully!\n", amount)
+
+	totalWater, err := calculateTotalWater(filename)
+	if err != nil {
+		fmt.Printf("Error calculating total water intake: %v\n", err)
+		return
+	}
+
+	if totalWater >= 1000 {
+		fmt.Printf("You've consumed %.2f litres today", float64(totalWater)/1000)
+	} else {
+		fmt.Printf("You've consumed %d ml today", totalWater)
+	}
 }
